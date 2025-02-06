b06516e1e6ce18c86b51c8a5e4a73f82de4185bc
EHMD28 <ehmdiagne0530@gmail.com>
2025-02-06 06:40:21 -0500

    added intake subsystem

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {}
---
+++ src/main/java/frc/robot/subsystems/intake/commands/Shoot.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Shoot extends Command {
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {}
+
+    @Override
+    public void end(boolean interrupted) {}
+}
